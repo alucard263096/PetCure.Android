17a505ff7cea594c77927170879bc94b9910b9a6
Steve Cai <steve.cai@morningstar.com>
2016-04-28 15:35:18 +0800

    添加等待测试

--- PetCure/app/src/main/java/com/helpfooter/steve/petcure/mgr/MapMgr.java
+++ PetCure/app/src/main/java/com/helpfooter/steve/petcure/mgr/MapMgr.java
@@ -45,13 +45,23 @@ public class MapMgr implements  TencentLocationListener {
     }
 
     @Override
-    public void onLocationChanged(TencentLocation tencentLocation, int i, String s) {
-        if (TencentLocation.ERROR_OK == i) {
+    public void onLocationChanged(TencentLocation location, int error,
+                                  String reason) {
+        String msg = null;
+        if (error == TencentLocation.ERROR_OK) {
             // 定位成功
-            Log.i("TencentLocation","OK");
+            StringBuilder sb = new StringBuilder();
+            sb.append("(纬度=").append(location.getLatitude()).append(",经度=")
+                    .append(location.getLongitude()).append(",精度=")
+                    .append(location.getAccuracy()).append("), 来源=")
+                    .append(location.getProvider()).append(", 地址=")
+                    .append(location.getAddress());
+            msg = sb.toString();
         } else {
-            Log.i("TencentLocation","FAIL");
+            // 定位失败
+            msg = "定位失败: " + reason;
         }
+        Toast.makeText(this.ctx, msg, Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -62,9 +72,9 @@ public class MapMgr implements  TencentLocationListener {
 			/* 检测到定位权限被内置或第三方的权限管理或安全软件禁用, 导致当前应用**很可能无法定位**
 			 * 必要时可对这种情况进行特殊处理, 比如弹出提示或引导
 			 */
-            Toast.makeText(this.ctx, "定位权限被禁用!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this.ctx, "定位权限被禁用!", Toast.LENGTH_LONG).show();
         }else {
-            Toast.makeText(this.ctx, message, Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this.ctx, message, Toast.LENGTH_LONG).show();
         }
     }
 
