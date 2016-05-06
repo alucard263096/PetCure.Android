6267aaa87c11458d5e8165ad385921af944a8dbf
Steve Cai <steve.cai@morningstar.com>
2016-05-06 16:39:20 +0800

    添加multiple-images-selector

--- PetCure/app/src/main/java/com/helpfooter/steve/petcure/PosterCreateActivity.java
+++ PetCure/app/src/main/java/com/helpfooter/steve/petcure/PosterCreateActivity.java
@@ -1,23 +1,73 @@
 package com.helpfooter.steve.petcure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.helpfooter.steve.petcure.mgr.ActivityMgr;
+import com.zfdang.multiple_images_selector.ImagesSelectorActivity;
+import com.zfdang.multiple_images_selector.SelectorSettings;
+
+import java.util.ArrayList;
 
 public class PosterCreateActivity extends AppCompatActivity {
 
+    public static class RequestCode{
+        public static int AddPhoto=1;
+    }
+    private ArrayList<String> mResults = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poster_create);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button bt = (Button) findViewById(R.id.button);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // start multiple photos selector
+                Intent intent = new Intent(PosterCreateActivity.this, ImagesSelectorActivity.class);
+                // max number of images to be selected
+                intent.putExtra(SelectorSettings.SELECTOR_MAX_IMAGE_NUMBER, 5);
+                // min size of image which will be shown; to filter tiny images (mainly icons)
+                intent.putExtra(SelectorSettings.SELECTOR_MIN_IMAGE_SIZE, 100);
+                // show camera or not
+                intent.putExtra(SelectorSettings.SELECTOR_SHOW_CAMERA, true);
+                // pass current selected images as the initial value
+                intent.putStringArrayListExtra(SelectorSettings.SELECTOR_INITIAL_SELECTED_LIST,mResults);
+                // start the selector
+                startActivityForResult(intent, RequestCode.AddPhoto);
+            }
+        });
+
     }
 
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // get selected images from selector
+        if(requestCode == RequestCode.AddPhoto) {
+            if(resultCode == RESULT_OK) {
+                mResults = data.getStringArrayListExtra(SelectorSettings.SELECTOR_RESULTS);
+                assert mResults != null;
+
+                // show results in textview
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format("Totally %d images selected:", mResults.size())).append("\n");
+                for(String result : mResults) {
+                    sb.append(result).append("\n");
+                }
+                Toast.makeText(PosterCreateActivity.this,sb.toString(),Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
